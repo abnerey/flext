eb021110b974bd514246a466705ef8fda896f4d0
Abner Juarez <abnerariel-1997@hotmail.com>
2018-06-19 14:46:51 -0600

    bugfix/beta2: bugfix for AOT

--- index.ts
+++ index.ts
@@ -14,6 +14,5 @@ export {
     FlextPromise,
     FlextHandler,
     FlextException,
-    FlextComponent,
-    Mixes
+    FlextComponent
 } from './src/decorators/index.decorators';
--- src/decorators/index.decorators.ts
+++ src/decorators/index.decorators.ts
@@ -1,4 +1,4 @@
-export {FlextComponent, Mixes} from './flext-component.decorator';
+export {FlextComponent} from './flext-component.decorator';
 export {FlextException, FlextHandler} from './flext-handler.decorator';
 export {FlextPromise} from './flext-promise.decorator';
 export {FlextSuccessStage, FlextErrorStage, FlextPostStage, FlextPreStage} from './flext-stages.decorator';
